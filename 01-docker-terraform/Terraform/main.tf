7fd38c5ec8c1ec856ca7533f574e953453a4c8c2
Gpetrolini <guspetrolini@hotmail.com>
2026-01-14 09:47:26 -0300

    refactor: restructure project folders and add terraform infra

---
+++ 01-docker-terraform/Terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "de-zoomcamp-484312"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "dtc-de-course-bucket-gustavo-2026"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = "demo_dataset"
+}
